8be780ca2c1e309eb3214dd4f8f04381b6cf6332
kei-seu <904812013@qq.com>
2022-10-08 15:17:32 +0800

    add UVM verification platform-cgm files

---
+++ kei_i2c_tb/cov/kei_i2c_cgm.sv
@@ -0,0 +1,23 @@
+
+
+`ifndef KEI_I2C_CGM_SV
+`define KEI_I2C_CGM_SV
+
+// Coverage Model
+class kei_i2c_cgm extends uvm_component;
+   
+  // TODO
+  // Covergroup definition below
+
+  // Analysis import declarion below
+
+  `uvm_component_utils(kei_i2c_cgm)
+
+  function new(string name = "kei_i2c_cgm", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+endclass
+
+
+`endif // KEI_I2C_CGM_SV
