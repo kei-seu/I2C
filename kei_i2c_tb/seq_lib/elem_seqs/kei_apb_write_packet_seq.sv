2b6b78c85083c623d963407670d33ce738ef4027
kei-seu <904812013@qq.com>
2022-10-10 11:11:40 +0800

    fix UVM verification platform-seq_lib-elem_seq files

--- kei_i2c_tb/seq_lib/elem_seqs/kei_apb_write_packet_seq.sv
+++ kei_i2c_tb/seq_lib/elem_seqs/kei_apb_write_packet_seq.sv
@@ -1,16 +1,16 @@
 
-`ifndef KEI_APB_READ_PACKET_SEQ_SV
-`define KEI_APB_READ_PACKET_SEQ_SV
+`ifndef KEI_APB_WRITE_PACKET_SEQ_SV
+`define KEI_APB_WRITE_PACKET_SEQ_SV
 
-class kei_apb_read_packet_seq extends kei_apb_base_sequence;
+class kei_apb_write_packet_seq extends kei_apb_base_sequence;
 
-  `uvm_object_utils(kei_apb_read_packet_seq)
+  `uvm_object_utils(kei_apb_write_packet_seq)
 
   constraint def_cstr {
     soft packet.size() == 1;
   }
 
-  function new (string name = "kei_apb_read_packet_seq");
+  function new (string name = "kei_apb_write_packet_seq");
     super.new(name);
   endfunction
 
@@ -19,21 +19,19 @@ class kei_apb_read_packet_seq extends kei_apb_base_sequence;
     super.body();
 
     foreach(packet[i]) begin
-      rgm.IC_DATA_CMD.CMD.set(RGM_READ); 
-      rgm.IC_DATA_CMD.DAT.set(0); 
-      rgm.IC_DATA_CMD.write(status, rgm.IC_DATA_CMD.get());
-    end
-
-    foreach(packet[i]) begin
-      // Wait until RX FIFO is not empty
+      // Wait until TX FIFO is not full 
       while(1) begin
         rgm.IC_STATUS.mirror(status);
-        if(rgm.IC_STATUS.RFNE.get() == 1) break;
+        if(rgm.IC_STATUS.TFNF.get() == 1) break;
         repeat(100) @(p_sequencer.vif.cb_mon);
       end
-      
-      rgm.IC_DATA_CMD.mirror(status);
-      packet[i] = rgm.IC_DATA_CMD.DAT.get();
+
+      rgm.IC_DATA_CMD.DAT.set(packet[i]);
+      rgm.IC_DATA_CMD.CMD.set(RGM_WRITE); 
+      // Use Frontdoor write instead of update() method to take 
+      // explicit write and avoid 'no-action' if mirror value 
+      // eqauls desired value
+      rgm.IC_DATA_CMD.write(status, rgm.IC_DATA_CMD.get());
     end
 
     `uvm_info("body", "Exiting...", UVM_HIGH)
@@ -41,4 +39,4 @@ class kei_apb_read_packet_seq extends kei_apb_base_sequence;
 
 endclass
 
-`endif // KEI_APB_READ_PACKET_SEQ_SV
+`endif // KEI_APB_WRITE_PACKET_SEQ_SV
--- kei_i2c_tb/seq_lib/elem_seqs/kei_i2c_slave_base_sequence.sv
+++ kei_i2c_tb/seq_lib/elem_seqs/kei_i2c_slave_base_sequence.sv
@@ -3,6 +3,13 @@
 `define KEI_I2C_SLAVE_BASE_SEQUENCE_SV
 
 virtual class kei_i2c_slave_base_sequence extends uvm_sequence #(kei_vip_i2c_slave_transaction);
+  
+  // WRITE/READ data packet content
+  rand bit [7:0] packet[];
+
+  rand int nack_addr = -1;
+  rand int nack_data = -1;
+  rand int nack_addr_count = -1;
 
   ral_block_kei_i2c rgm;
   kei_vip_i2c_configuration cfg;
@@ -18,6 +25,10 @@ virtual class kei_i2c_slave_base_sequence extends uvm_sequence #(kei_vip_i2c_sla
   endfunction
 
   virtual task body();
+    if(!uvm_config_db #(ral_block_kei_i2c)::get(m_sequencer, "", "rgm", rgm)) begin
+      `uvm_error("body", "Unable to find ral_block_kei_i2c in uvm_config_db")
+    end
+    p_sequencer.get_cfg(cfg);
     // TODO
     // Attach element sequences below
   endtask
