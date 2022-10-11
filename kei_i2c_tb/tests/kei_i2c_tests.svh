9c94d5ad7c644785041e4289c5596bf945d1d537
kei-seu <904812013@qq.com>
2022-10-11 20:22:17 +0800

    update UVM verification platform-test-kei_i2c_tests.svh

--- kei_i2c_tb/tests/kei_i2c_tests.svh
+++ kei_i2c_tb/tests/kei_i2c_tests.svh
@@ -7,5 +7,8 @@
 `include "kei_i2c_master_directed_interrupt_test.sv"
 `include "kei_i2c_master_directed_write_packet_test.sv"
 `include "kei_i2c_master_directed_read_packet_test.sv"
+`include "kei_i2c_reg_access_test.sv"
+`include "kei_i2c_reg_bit_bash_test.sv"
+`include "kei_i2c_reg_hw_reset_test.sv"
 
 `endif // KEI_I2C_TESTS_SVH
