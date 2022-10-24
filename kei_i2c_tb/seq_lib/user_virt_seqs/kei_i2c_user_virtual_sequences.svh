baf87e953ac3b024b5b773f79916230700cb4eda
kei-seu <904812013@qq.com>
2022-10-24 22:37:29 +0800

    update UVM verification platform-seq_lib-user_virt_seq-kei_i2c_user_virtual_sequences.svh

--- kei_i2c_tb/seq_lib/user_virt_seqs/kei_i2c_user_virtual_sequences.svh
+++ kei_i2c_tb/seq_lib/user_virt_seqs/kei_i2c_user_virtual_sequences.svh
@@ -2,6 +2,8 @@
 `ifndef KEI_I2C_USER_VIRTUAL_SEQUENCES_SVH
 `define KEI_I2C_USER_VIRTUAL_SEQUENCES_SVH
 
+`include "kei_i2c_master_address_cg_virt_seq.sv"
+
 
 `endif // KEI_I2C_USER_VIRTUAL_SEQUENCES_SVH
 
