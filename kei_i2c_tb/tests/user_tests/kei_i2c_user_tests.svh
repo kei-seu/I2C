e070fd1bf4fb5cee9122124d06b30a9160c4fc2e
kei-seu <904812013@qq.com>
2022-10-25 17:26:18 +0800

    update UVM verification platform-test-user_test-kei_i2c_user_tests.svh

--- kei_i2c_tb/tests/user_tests/kei_i2c_user_tests.svh
+++ kei_i2c_tb/tests/user_tests/kei_i2c_user_tests.svh
@@ -4,5 +4,9 @@
 
 `include "kei_i2c_master_address_cg_test.sv"
 
+`include "kei_i2c_master_ss_cnt_test.sv"
+`include "kei_i2c_master_fs_cnt_test.sv"
+`include "kei_i2c_master_hs_cnt_test.sv"
+
 `endif // KEI_I2C_USER_TESTS_SVH
 
