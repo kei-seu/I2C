0985510cc529f0652bbf200bd01ddd0a31996d38
kei-seu <904812013@qq.com>
2022-10-30 21:17:16 +0800

    update UVM verification platform-seq_lib-user_elem_seq-kei_i2c_user_element_sequences.svh

--- kei_i2c_tb/seq_lib/user_elem_seqs/kei_i2c_user_element_sequences.svh
+++ kei_i2c_tb/seq_lib/user_elem_seqs/kei_i2c_user_element_sequences.svh
@@ -2,6 +2,7 @@
 `ifndef KEI_I2C_USER_ELEMENT_SEQUENCES_SVH
 `define KEI_I2C_USER_ELEMENT_SEQUENCES_SVH
 
+`include "kei_apb_write_nocheck_packet_seq.sv"
 
 `endif // KEI_I2C_USER_ELEMENT_SEQUENCES_SVH
 
