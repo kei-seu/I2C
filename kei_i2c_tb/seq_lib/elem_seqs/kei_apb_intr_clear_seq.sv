e34c7214465add9279debad7f882c10d79a4454b
kei-seu <904812013@qq.com>
2022-11-01 23:01:34 +0800

    fix UVM verification platform-seq_lib-elem_seq-kei_apb_intr_clear_seq.sv

--- kei_i2c_tb/seq_lib/elem_seqs/kei_apb_intr_clear_seq.sv
+++ kei_i2c_tb/seq_lib/elem_seqs/kei_apb_intr_clear_seq.sv
@@ -19,21 +19,21 @@ class kei_apb_intr_clear_seq extends kei_apb_base_sequence;
 
     rgm.IC_RAW_INTR_STAT.mirror(status);
     case(intr_id)
-      IC_RX_OVER_INTR_ID         : rgm.IC_CLR_RX_OVER.write(status, 'b1);
-      IC_RX_UNDER_INTR_ID        : rgm.IC_CLR_RX_UNDER.write(status, 'b1);
-      IC_TX_OVER_INTR_ID         : rgm.IC_CLR_TX_OVER.write(status, 'b1);
-      IC_TX_ABRT_INTR_ID         : rgm.IC_CLR_TX_ABRT.write(status, 'b1); 
-      IC_RX_DONE_INTR_ID         : rgm.IC_CLR_RX_DONE.write(status, 'b1); 
+      IC_RX_OVER_INTR_ID         : rgm.IC_CLR_RX_OVER.mirror(status);
+      IC_RX_UNDER_INTR_ID        : rgm.IC_CLR_RX_UNDER.mirror(status);
+      IC_TX_OVER_INTR_ID         : rgm.IC_CLR_TX_OVER.mirror(status);
+      IC_TX_ABRT_INTR_ID         : rgm.IC_CLR_TX_ABRT.mirror(status); 
+      IC_RX_DONE_INTR_ID         : rgm.IC_CLR_RX_DONE.mirror(status); 
       //IC_TX_EMPTY_INTR_ID        : NOTE:: NO REG; 
-      IC_ACTIVITY_INTR_ID        : rgm.IC_CLR_ACTIVITY.write(status, 'b1); 
-      IC_STOP_DET_INTR_ID        : rgm.IC_CLR_STOP_DET.write(status, 'b1); 
-      IC_START_DET_INTR_ID       : rgm.IC_CLR_START_DET.write(status, 'b1); 
-      IC_RD_REQ_INTR_ID          : rgm.IC_CLR_RD_REQ.write(status, 'b1); 
+      IC_ACTIVITY_INTR_ID        : rgm.IC_CLR_ACTIVITY.mirror(status); 
+      IC_STOP_DET_INTR_ID        : rgm.IC_CLR_STOP_DET.mirror(status); 
+      IC_START_DET_INTR_ID       : rgm.IC_CLR_START_DET.mirror(status); 
+      IC_RD_REQ_INTR_ID          : rgm.IC_CLR_RD_REQ.mirror(status); 
       //IC_RX_FULL_INTR_ID         : NOTE:: NO REG;  
-      IC_GEN_CALL_INTR_ID        : rgm.IC_CLR_GEN_CALL.write(status, 'b1); 
+      IC_GEN_CALL_INTR_ID        : rgm.IC_CLR_GEN_CALL.mirror(status); 
       //IC_RESTART_DET_INTR_ID     : NOTE:: NO REG;    
       //IC_MASTER_ON_HOLD_INTR_ID  : NOTE:: NO REG;   
-      IC_ALL_INTR_ID             : rgm.IC_CLR_INTR.write(status, 'b1);
+      IC_ALL_INTR_ID             : rgm.IC_CLR_INTR.mirror(status);
       default : `uvm_error("INTRCL", $sformatf("The interrupt id [%0d] could not be clear via software register", intr_id))
     endcase
     repeat(100) @(p_sequencer.vif.cb_mon);
