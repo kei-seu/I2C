3d546a98fe6e6a6f83fd8f7e5108f24489aac5bf
kei-seu <904812013@qq.com>
2022-11-04 21:27:34 +0800

    add UVM verification platform-test-user_test-kei_i2c_master_*_intr_test.sv(aim at vplan5.1-3)

---
+++ kei_i2c_tb/tests/user_tests/kei_i2c_master_rx_full_intr_test.sv
@@ -0,0 +1,30 @@
+
+`ifndef KEI_I2C_MASTER_RX_FULL_INTR_TEST_SV
+`define KEI_I2C_MASTER_RX_FULL_INTR_TEST_SV
+
+class kei_i2c_master_rx_full_intr_test extends kei_i2c_base_test;
+
+  `uvm_component_utils(kei_i2c_master_rx_full_intr_test)
+
+  function new(string name = "kei_i2c_master_rx_full_intr_test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    // TODO
+    // modify components' configurations
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    kei_i2c_master_rx_full_intr_virt_seq seq = kei_i2c_master_rx_full_intr_virt_seq::type_id::create("seq");
+    phase.raise_objection(this);
+    `uvm_info("SEQ", "sequence starting", UVM_LOW)
+    seq.start(env.sqr);
+    `uvm_info("SEQ", "sequence finished", UVM_LOW)
+    phase.drop_objection(this);
+  endtask
+
+endclass
+
+`endif // KEI_I2C_MASTER_RX_FULL_INTR_TEST_SV
