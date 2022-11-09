793e0844bc0446d10f6cb93a40523d3a35fb4d88
kei-seu <904812013@qq.com>
2022-11-09 22:03:14 +0800

    update UVM verification platform-seq_lib-elem_seq-kei_apb_config_seq.sv

--- kei_i2c_tb/seq_lib/elem_seqs/kei_apb_config_seq.sv
+++ kei_i2c_tb/seq_lib/elem_seqs/kei_apb_config_seq.sv
@@ -9,6 +9,7 @@ class kei_apb_config_seq extends kei_apb_base_sequence;
   constraint def_cstr {
     soft SPEED == -1;
     soft IC_10BITADDR_MASTER == -1;
+    soft IC_RESTART_EN == -1;
     soft IC_TAR == -1;
     soft IC_SS_SCL_HCNT == -1;
     soft IC_SS_SCL_LCNT == -1;
@@ -18,6 +19,8 @@ class kei_apb_config_seq extends kei_apb_base_sequence;
     soft IC_HS_SCL_LCNT == -1;
     soft ENABLE == -1;
     soft IC_SAR == -1;
+    soft SPECIAL == -1;
+    soft GC_OR_START == -1;
   }
 
   function new (string name = "kei_apb_config_seq");
@@ -31,9 +34,12 @@ class kei_apb_config_seq extends kei_apb_base_sequence;
     if(SPEED >= 0) rgm.IC_CON.SPEED.set(SPEED);
     //rgm.IC_CON.MASTER_MODE.set('h1);
     if(IC_10BITADDR_MASTER >= 0) rgm.IC_CON.IC_10BITADDR_MASTER.set(IC_10BITADDR_MASTER);
+    if(IC_RESTART_EN >= 0) rgm.IC_CON.IC_RESTART_EN.set(IC_RESTART_EN);
     rgm.IC_CON.update(status);
 
     if(IC_TAR >= 0) rgm.IC_TAR.IC_TAR.set(IC_TAR);
+    if(SPECIAL >= 0) rgm.IC_TAR.SPECIAL.set(SPECIAL);
+    if(GC_OR_START >= 0) rgm.IC_TAR.GC_OR_START.set(GC_OR_START);
     rgm.IC_TAR.update(status);
 
     if(IC_SAR >= 0) rgm.IC_SAR.IC_SAR.set(IC_SAR);
