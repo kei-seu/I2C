c22d528bcf70169afd1e687254c2aec0ee0f7065
kei-seu <904812013@qq.com>
2022-11-16 11:43:38 +0800

    update UVM verification platform-test-user_test-kei_i2c_user_tests.svh

--- kei_i2c_tb/tests/user_tests/kei_i2c_user_tests.svh
+++ kei_i2c_tb/tests/user_tests/kei_i2c_user_tests.svh
@@ -16,17 +16,24 @@
 
 `include "kei_i2c_master_stop_det_intr_test.sv"
 `include "kei_i2c_master_tx_abrt_intr_test.sv"
+`include "kei_i2c_master_tx_over_intr_test.sv"
 `include "kei_i2c_master_rx_full_intr_test.sv"
 `include "kei_i2c_master_rx_over_intr_test.sv"
+`include "kei_i2c_master_rx_under_intr_test.sv"
 
 `include "kei_i2c_master_10b_rd_norstrt_abrt_test.sv"
 `include "kei_i2c_master_sbyte_norstrt_abrt_test.sv"
 `include "kei_i2c_master_txdata_noack_abrt_test.sv"
 `include "kei_i2c_master_7b_addr_noack_abrt_test.sv"
 `include "kei_i2c_master_10b_addr1_noack_abrt_test.sv"
+`include "kei_i2c_master_10b_addr2_noack_abrt_test.sv"
 `include "kei_i2c_master_hs_norstrt_abrt_test.sv"
 `include "kei_i2c_master_gcall_noack_abrt_test.sv"
 `include "kei_i2c_master_gcall_read_abrt_test.sv"
+`include "kei_i2c_master_sbyte_ackdet_abrt_test.sv"
+`include "kei_i2c_master_hs_ackdet_abrt_test.sv"
+`include "kei_i2c_master_arb_lost_abrt_test.sv"
+`include "kei_i2c_master_master_dis_abrt_test.sv"
 
 `endif // KEI_I2C_USER_TESTS_SVH
 
